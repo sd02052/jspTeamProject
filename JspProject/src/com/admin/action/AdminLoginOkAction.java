1ff9f04ade4f48fbe90da98b88d83ddf89fdd2c8
SOS <SOS@DESKTOP-T28PJOJ>
2021-06-02 14:31:27 +0900

    폴더생성 테스트

---
+++ JspProject/src/com/admin/action/AdminLoginOkAction.java
@@ -0,0 +1,19 @@
+package com.admin.action;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.okky.controller.Action;
+import com.okky.controller.ActionForward;
+
+public class AdminLoginOkAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

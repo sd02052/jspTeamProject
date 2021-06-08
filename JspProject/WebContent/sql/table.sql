070796d729cb40bd5cafd6ed6638665bd371ff4f
csooy <csooy38@gmail.com>
2021-06-08 09:14:25 +0900

    DB 수정(company/job) 및 샘플 레코드, 테이블 삭제 sql문 추가

--- JspProject/WebContent/sql/table.sql
+++ JspProject/WebContent/sql/table.sql
@@ -33,7 +33,7 @@ create table okky_board(
     board_hit number(5) default 0,			-- 게시글 조회 수
     board_like number(5) default 0,			-- 게시글 추천 수
     board_scrap number(5) default 0,		-- 게시글 스크랩 수
-    board_category number(5) not null,	-- 게시글 카테고리
+    board_category number(5) not null,		-- 게시글 카테고리
     board_regdate date,						-- 게시글 작성일자
     constraint fk_writer foreign key(board_writer) references okky_member(mem_num) on delete cascade,
     constraint fk_category foreign key(board_category) references okky_category(cate_num) on delete cascade
@@ -85,18 +85,19 @@ create table okky_company(
 	company_charge_phone varchar2(100) not null,	-- 회사 담당자 번호
 	company_charge_email varchar2(100) not null,	-- 회사 담당자 메일
 	company_charge_name varchar2(100) not null,		-- 회사 담당자 이름
-	company_emp number(4) default 0,				-- 회사 직원 수
+	company_emp varchar2(100),				        -- 회사 직원 수
 	company_homepage varchar2(100),					-- 회사 홈페이지
 	company_logo varchar2(200),						-- 회사 로고
 	company_content varchar2(1000) not null,		-- 회사 소개
 	company_check number(1) default 0,				-- 회사 등록 인증 여부(0: 미수락, 1: 수락)
-	company_regdate date							-- 회사 등록일자
+	company_target number(5) not null,				-- 회사 등록 게시글 번호
+    constraint fk_target5 foreign key(company_target) references okky_board(board_num) on delete cascade 
 );
 
 create table okky_job(
-    job_num number(5) primary key,		    -- 구인 게시글 번호
+    job_num number(5) primary key,		   -- 구인 게시글 번호
 	job_target number(5) not null,         -- 구인 게시글 연결 번호
-    job_contract varchar2(50) not null,    -- 구인 계약형태(정규직/계약직)
+    job_contract number(5) not null,       -- 구인 계약형태(0:정규직/1:계약직)
     job_mincost number(10) not null,       -- 최소 급여
     job_maxcost number(10) not null,       -- 최대 급여
     job_location varchar2(50) not null,    -- 지역
